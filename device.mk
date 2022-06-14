1059a34e1440d3943281f0efa2a5f04f71b08a40
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-06-14 14:40:32 +0530

    Alioth: Ship camera config in tree
    - From Miui Alioth 14.0.8.0 Mi
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Miui Camera STLicense
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
