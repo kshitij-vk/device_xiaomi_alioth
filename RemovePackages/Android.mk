c314a72f2b4c90ab9a7bb52323c46cc633b2231f
Ghosuto <clash.raja10@gmail.com>
2023-05-12 09:29:20 +0000

    alioth: Add burn-in protection for statusbar and navbar
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,21 +5,52 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AudioFX \
+    Eleven \
+    Etar \
+    ExactCalculator \
+    Jelly \
+    MusicFX \
+    Music \
+    Recorder \
+    Seedvault \
+    Chrome \
+    Chrome-Stub \
     Drive \
+    DeskClock \
     GoogleCamera \
+    Glimpse \
+    Gallery2 \
     Maps \
+    MiuiScanner \
+    Matlog \
+    MatLog \
     MyVerizonServices \
     OBDM_Permissions \
+    GoogleTTS \
+    arcore \
+    Videos \
+    MaestroPrebuilt \
+    talkback \
+    SoundAmplifierPrebuilt \
+    DevicePolicyPrebuilt \
+    TrichromeLibrary \
+    TrichromeWebView \
+    TrichromeChrome \
     obdm_stub \
     OemDmTrigger \
     OPScreenRecord \
     Ornament \
+    Photos \
+    PrebuiltGmail \
     SafetyHubPrebuilt \
     ScribePrebuilt \
     Showcase \
     Snap \
     SprintDM \
     SprintHM \
+    ViaBrowser \
+    Videos \
     VZWAPNLib \
     VzwOmaTrigger \
     YouTube \
