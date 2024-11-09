23f7751b1b862eb9541364cd658a020a924f9375
Kshitij Koyande <68855313+Roxor-007@users.noreply.github.com>
2024-11-09 21:49:34 +0530

    alioth: Include miui camera
    - Switch to jemalloc for libc
    - Use TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT
    - Set TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED
    
    Signed-off-by: Kshitij Koyande <68855313+Roxor-007@users.noreply.github.com>
    Co-Authored-By: Shubham Mishra <mishra.shubham5208@gmail.com>
    Co-Authored-By: Rohit Roy <rohitroy1902@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ TARGET_IS_VAB := true
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
+# Miuicamera
+$(call inherit-product, vendor/xiaomi/camera/miuicamera.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
