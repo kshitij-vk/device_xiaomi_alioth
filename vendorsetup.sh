31b38ee61e5e7676e2a613a12cf8db9cb95360de
Kshitij Koyande <68855313+Roxor-007@users.noreply.github.com>
2024-11-09 21:35:19 +0530

    alioth: Add neutron clang to the vendorsetup.sh
    - clone kernel and setup kernelsu
    - Patch glibc if Arch Linux is not found

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+
+# Clone kernel && setup kernelsu submodule
+git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250.git -b staging kernel/xiaomi/sm8250;
+cd kernel/xiaomi/sm8250;
+git submodule init;
+git submodule update;
+cd ../../../;
+
+# Setup clang and antman
+mkdir -p prebuilts/clang/host/linux-x86/clang-neutron
+cd prebuilts/clang/host/linux-x86/clang-neutron
+curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
+chmod +x antman
+./antman -S
+
+
+# Check if the system is not Arch Linux before running the patch command
+if ! grep -q "Arch" /etc/os-release; then
+  ./antman --patch=glibc
+fi
+
+cd ../../../../../
