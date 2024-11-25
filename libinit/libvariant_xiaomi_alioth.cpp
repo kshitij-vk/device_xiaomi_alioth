3de55b51c72ee1f2f1e2d4f54f2eca2f8996fe11
darkphnx <dark.phnx12@gmail.com>
2024-11-25 13:19:33 +0000

    fixup! fix MIUI Cam for Mi11x and K40

--- libinit/libvariant_xiaomi_alioth.cpp
+++ libinit/libvariant_xiaomi_alioth.cpp
@@ -9,9 +9,7 @@
 static const variant_info aliothin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
-
-    .mod_device = "aliothin",
-    .name = "aliothin",
+    .mod_device = "alioth_in_global",
     .brand = "Mi",
     .device = "aliothin",
     .marketname = "Mi 11X",
@@ -24,9 +22,7 @@ static const variant_info aliothin_info = {
 static const variant_info alioth_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
-
-    .mod_device = "alioth_global",
-    .name = "alioth_global",
+    .mod_device = "alioth_in_global",
     .brand = "POCO",
     .device = "alioth",
     .marketname = "POCO F3",
@@ -39,9 +35,7 @@ static const variant_info alioth_global_info = {
 static const variant_info alioth_info = {
     .hwc_value = "",
     .sku_value = "",
-
-    .mod_device = "alioth",
-    .name = "alioth",
+    .mod_device = "alioth_in_global",
     .brand = "Redmi",
     .device = "alioth",
     .marketname = "Redmi K40",
