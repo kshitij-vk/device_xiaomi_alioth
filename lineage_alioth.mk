43255a8777141abccd83e88337ec67de8b3ed599
kshitij-vk <koyande72@gmail.com>
2024-12-30 11:04:19 +0530

    alioth: Tenx-OS Init

---
+++ lineage_alioth.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit Camera-related flags
+#TARGET_USES_MIUI_CAMERA := true
+#TARGET_INCLUDES_MIUI_CAMERA := true
+
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+
+PRODUCT_NAME := lineage_alioth
+PRODUCT_DEVICE := alioth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.2.0.TKHMIXM:user/release-keys
+
+TARGET_BOOT_ANIMATION_RES := 1440
+TENX_BUILD_TYPE := Official
+WITH_GMS := true
+
+# Pixel stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_QUICK_TAP := true
